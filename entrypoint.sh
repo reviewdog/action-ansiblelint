210dc7efe0fd62073d38e3c6e9b473a2e4d0a76a
Kaito Akita <kaitokun07@icloud.com>
2020-08-14 02:38:56 +0900

    fix error format

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ cd "$GITHUB_WORKSPACE" || exit
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
 ansible-lint -p ${INPUT_ANSIBLELINT_FLAGS}\
-  | reviewdog -efm="%f:%l:%c: %m" \
+  | reviewdog -efm="%f:%l: %m" \
       -name="ansible-lint" \
       -reporter="${INPUT_REPORTER:-github-pr-check}" \
       -level="${INPUT_LEVEL}" \
