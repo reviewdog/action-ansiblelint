b7f92e3663779581e61fe5837cc576f3e3714a87
takezyou <kaito.akita@zozo.com>
2020-08-13 20:41:23 +0900

    deleted flags

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ cd "$GITHUB_WORKSPACE" || exit
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-ansible-lint -p ${INPUT_ANSIBLELINT_FLAGS} \
+ansible-lint -p \
   | reviewdog -efm="%f:%l:%c: %m" \
       -name="ansible-lint" \
       -reporter="${INPUT_REPORTER:-github-pr-check}" \
